7a5f44727a1d1e5e680c9e031998566ad6cdef0d
Yulia_Iakovleva <Yulia_Iakovleva@epam.com>
2022-03-28 11:44:50 +0200

    Password complexity

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 # Java Code Challenges
 
-## Verify If a Number Is Even
+### Verify If a Number Is Even or Odd
 * If number is even, function returns "true"
 * If number is odd, function returns "false"
 * The input must be an integer
+
+### Password Complexity
+Verification of password:
+* Must have at least six characters
+* Must have one uppercase letter
+* Must have one lowercase letter
+* Must have one number
+
+Function needs
+* String as input
+* Boolean output indicates if password is complex enough
---
+++ src/main/Java/PasswordComplexity.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class PasswordComplexity {
+
+    public static boolean isPasswordComplex(String password) {
+
+        if (password.length() < 6) {
+            return false;
+        }
+
+        boolean hasLowercaseLetter = false;
+        boolean hasUppercaseLetter = false;
+        boolean hasNumber = false;
+
+        for (int i = 0; i < password.length() ||
+                (!hasLowercaseLetter
+                        && !hasUppercaseLetter
+                        && !hasNumber); i++) {
+
+            char current = password.charAt(i);
+
+            if (Character.isDigit(current)) {
+                hasNumber = true;
+            } else if (Character.isUpperCase(current)) {
+                hasLowercaseLetter = true;
+            } else if (Character.isLowerCase(current)) {
+                hasUppercaseLetter = true;
+            }
+        }
+
+        return hasNumber && hasLowercaseLetter && hasUppercaseLetter;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a password: ");
+        String userInput = scanner.nextLine();
+        System.out.println("Is the password complex? " +
+                isPasswordComplex(userInput));
+    }
+}
